3ac1378c85050b459d34a058b96de249ac3deafa
Tobias Wojtylak <twojtylak@dfau.de>
2025-02-28 14:24:38 +0100

    fix: use longtext & longblob instead of text for mail fields (data truncated error)

--- ext_tables.sql
+++ ext_tables.sql
@@ -12,11 +12,11 @@ CREATE TABLE tx_sentmail_mail (
     message_id varchar(255) DEFAULT '' NOT NULL,
     internal_id varchar(255) DEFAULT '' NOT NULL,
     is_sent tinyint(1) DEFAULT '0' NOT NULL,
-    debug text DEFAULT '' NOT NULL,
-    message text DEFAULT '' NOT NULL,
-    message_original text DEFAULT '' NOT NULL,
-    original_message text DEFAULT '' NOT NULL,
-    envelope_original text DEFAULT '' NOT NULL,
-    email_serialized text DEFAULT '' NOT NULL,
-    settings text DEFAULT '' NOT NULL
+    debug longtext DEFAULT '' NOT NULL,
+    message longtext DEFAULT '' NOT NULL,
+    message_original longtext DEFAULT '' NOT NULL,
+    original_message longtext DEFAULT '' NOT NULL,
+    envelope_original longblob DEFAULT '' NOT NULL,
+    email_serialized longblob DEFAULT '' NOT NULL,
+    settings longtext DEFAULT '' NOT NULL
 );
